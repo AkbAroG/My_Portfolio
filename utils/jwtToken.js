9dd73c075839464270f02358fed9f60d9b2d332f
akbar ali <akbaroofficial041@gmail.com>
2026-04-27 18:18:08 +0500

    latest fix

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,12 +1,31 @@
-//  export const generateToken = (user, message, statusCode, res) => {
+// //  export const generateToken = (user, message, statusCode, res) => {
+// //   const token = user.generateJsonWebToken();
+// //   res
+// //     .status(statusCode)
+// //     .cookie("token", token, {
+// //       expires: new Date(
+// //         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+// //       ),
+// //       httpOnly: true,
+// //     })
+// //     .json({
+// //       success: true,
+// //       message,
+// //       user,
+// //       token,
+// //     });
+// // };
+// export const generateToken = (user, message, statusCode, res) => {
 //   const token = user.generateJsonWebToken();
-//   res
-//     .status(statusCode)
+
+//   res.status(statusCode)
 //     .cookie("token", token, {
+//       httpOnly: true,           // JS cannot access cookie
+//       secure: false,            // Localhost ke liye false (HTTPS nahi)
+//       sameSite: "none",       // Cross-origin dashboard/posts (axios withCredentials)
 //       expires: new Date(
 //         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
 //       ),
-//       httpOnly: true,
 //     })
 //     .json({
 //       success: true,
@@ -15,14 +34,15 @@
 //       token,
 //     });
 // };
+
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
 
   res.status(statusCode)
     .cookie("token", token, {
-      httpOnly: true,           // JS cannot access cookie
-      secure: false,            // Localhost ke liye false (HTTPS nahi)
-      sameSite: "none",       // Cross-origin dashboard/posts (axios withCredentials)
+      httpOnly: true,
+      secure: true,          // 🔥 MUST be true on Vercel (HTTPS)
+      sameSite: "none",      // required for cross-domain frontend/backend
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
